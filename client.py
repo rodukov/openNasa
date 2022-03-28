4c9ce2a518e5c7bacfad62b1d230027ed69735db
Gleb Rodukov <101352977+rodukov@users.noreply.github.com>
2022-03-28 21:35:50 +0300

    client.py; nasa_scrape.py
    Master files

---
+++ client.py
@@ -0,0 +1,65 @@
+import argparse
+from datetime import datetime
+
+from aiohttp import request
+from nasa_scrape import Scrape
+from random import randint, choice
+
+
+parser = argparse.ArgumentParser(description='NASA Scraper argument parser')
+parser.add_argument('-k', '--key', type=str)
+parser.add_argument('-b', '--build', type=bool)
+parser.add_argument('-y', '--year')
+parser.add_argument('-m', '--month')
+parser.add_argument('-d', '--day')
+parser.add_argument('-c', '--count')
+client_args = parser.parse_args()
+
+_logos = [""""
+ _______  _______  _______  __    _  __    _  _______  _______  _______ 
+|       ||       ||       ||  |  | ||  |  | ||   _   ||       ||   _   |
+|   _   ||    _  ||    ___||   |_| ||   |_| ||  |_|  ||  _____||  |_|  |
+|  | |  ||   |_| ||   |___ |       ||       ||       || |_____ |       |
+|  |_|  ||    ___||    ___||  _    ||  _    ||       ||_____  ||       |
+|       ||   |    |   |___ | | |   || | |   ||   _   | _____| ||   _   |
+|_______||___|    |_______||_|  |__||_|  |__||__| |__||_______||__| |__|
+
+""","""
+  ____                _  __             
+ / __ \___  ___ ___  / |/ /__ ____ ___ _
+/ /_/ / _ \/ -_) _ \/    / _ `(_-</ _ `/
+\____/ .__/\__/_//_/_/|_/\_,_/___/\_,_/ 
+    /_/                     
+""", """
+   ___                 _  _               
+  / _ \ _ __  ___ _ _ | \| |__ _ ___ __ _ 
+ | (_) | '_ \/ -_) ' \| .` / _` (_-</ _` |
+  \___/| .__/\___|_||_|_|\_\__,_/__/\__,_|
+       |_|                                
+
+"""]
+_history = []
+print(choice(_logos))
+nasa = Scrape(api_key=client_args.key)
+if client_args.build != True:
+    nasa.build(start=True)
+for i in range(int(client_args.count)):
+    request_data = []
+    if (client_args.year == "random"): request_data.append(randint(1996, int((str(datetime.now()).split(" ")[0]).split("-")[0])))
+    else: request_data.append(client_args.year)
+    if (client_args.month == "random"): request_data.append(randint(1, 12))
+    else: request_data.append(client_args.year)
+    if (client_args.day == "random"): request_data.append(randint(1, 31))
+    else: request_data.append(client_args.year)
+    
+    output = nasa.request(year=str(request_data[0]), month=str(request_data[1]), day=str(request_data[2]))
+    if output not in _history:
+        _history.append(output)
+        if client_args.build != True:
+            nasa.build(output)
+            print(1)
+        else:
+            print(output)
+else:
+    if client_args.build != True:
+        nasa.build(end=True)
---
+++ nasa_scrape.py
@@ -0,0 +1,44 @@
+from requests import get
+from json import loads
+from time import time
+from random import randint, choice
+
+
+class Scrape:
+    def __init__(self, api_key:str) -> None:
+        self.api_key = api_key
+        self.time = round(time(),0)
+    def request(self, year:str, month:str, day:str) -> dict:
+        linksrc = f"https://api.nasa.gov/planetary/apod?date={year}-{month}-{day}&hd=True&api_key={self.api_key}"
+        data = loads(get(linksrc).text)
+        return data
+    def build(self, data:dict={}, start:bool=False, end:bool=False) -> bool:
+        CHOICE = [["45A6C1", "46C2A3"], ["4671C2", "8246C2"], ["C24646", "FFA630"]]
+        COLOR = choice(CHOICE)
+
+        URL = str(data["url"]) if "url" in data else ""
+        TITLE = str(data["title"]) if "title" in data else ""
+        EXPLANATION = str(data["explanation"]) if "explanation" in data else ""
+        DATE = str(data["date"]) if "date" in data else ""
+
+        with open(f"index-{self.time}.html", "a") as htmlpage:
+            if start:
+                htmlpage.write("""<!DOCTYPE HTML>
+            <html>
+            <head>
+                <title>NASA Pictures</title>
+            </head>
+            <body>
+                """)
+            elif end:
+                htmlpage.write("""
+            </body>
+                </html>""")
+            else:
+                htmlpage.write("""
+                <div style="border-radius: 10px; background: white; border: 1px solid #""" + COLOR[0] + """; padding: 25px; margin: 10px;">
+                    <div><p style="text-align: center; margin: 20px;"><img style="border-radius: 5px;" src='""" + URL + """'></p></div>
+                    <div><h1 style="font-family: arial; text-align: center; color: #282828;">""" + TITLE + """</h1><h3 style="font-family: arial; text-align: center; color: #868686;">""" + DATE + """</h3></div>
+                    <div style="color: white; padding: 10px; margin: 10px; border-radius: 20px; box-shadow: 0px 5px 13px #AFAFAF; font-family: arial; background: linear-gradient(45deg, #""" + COLOR[0] + """, #""" + COLOR[1] + """)"><p>""" + EXPLANATION + """</p></div>
+                </div>
+                """)
