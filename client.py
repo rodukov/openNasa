970d228e3ad2b3ed126a112062af06a767885306
rodukov <electroneuphoria@gmail.com>
2022-03-30 20:56:11 +0300

    optimization

--- client.py
+++ client.py
@@ -1,65 +1,49 @@
-import argparse
+from json import loads
+from argparse import ArgumentParser
 from datetime import datetime
-
-from aiohttp import request
-from nasa_scrape import Scrape
 from random import randint, choice
+from modules.nasa_scrape import Scrape
+from modules.config import _logos
 
 
-parser = argparse.ArgumentParser(description='NASA Scraper argument parser')
-parser.add_argument('-k', '--key', type=str)
-parser.add_argument('-b', '--build', type=bool)
-parser.add_argument('-y', '--year')
-parser.add_argument('-m', '--month')
-parser.add_argument('-d', '--day')
-parser.add_argument('-c', '--count')
+"""Getting user arguments"""
+parser = ArgumentParser(description='Information about using the openNasa software')
+parser.add_argument('-k', '--key', help="NASA API key")
+parser.add_argument('-b', '--build', help="If True the script will create an html page, if False it will output the results to the terminal")
+parser.add_argument('-y', '--year', help="Enter the desired year, for random results write 'random'")
+parser.add_argument('-m', '--month', help="Enter the desired month, for random results write 'random'")
+parser.add_argument('-d', '--day', help="Enter the desired day, for random results write 'random'")
+parser.add_argument('-c', '--count', help="Enter the number of articles, for example, '30', without quotes")
 client_args = parser.parse_args()
 
-_logos = [""""
- _______  _______  _______  __    _  __    _  _______  _______  _______ 
-|       ||       ||       ||  |  | ||  |  | ||   _   ||       ||   _   |
-|   _   ||    _  ||    ___||   |_| ||   |_| ||  |_|  ||  _____||  |_|  |
-|  | |  ||   |_| ||   |___ |       ||       ||       || |_____ |       |
-|  |_|  ||    ___||    ___||  _    ||  _    ||       ||_____  ||       |
-|       ||   |    |   |___ | | |   || | |   ||   _   | _____| ||   _   |
-|_______||___|    |_______||_|  |__||_|  |__||__| |__||_______||__| |__|
+_history = [] # stores all requests
+print(choice(_logos)) # outputs a random logo from the config file
 
-""","""
-  ____                _  __             
- / __ \___  ___ ___  / |/ /__ ____ ___ _
-/ /_/ / _ \/ -_) _ \/    / _ `(_-</ _ `/
-\____/ .__/\__/_//_/_/|_/\_,_/___/\_,_/ 
-    /_/                     
-""", """
-   ___                 _  _               
-  / _ \ _ __  ___ _ _ | \| |__ _ ___ __ _ 
- | (_) | '_ \/ -_) ' \| .` / _` (_-</ _` |
-  \___/| .__/\___|_||_|_|\_\__,_/__/\__,_|
-       |_|                                
+def client(nasa) -> None:
+    if loads(client_args.build.lower()):
+        nasa.build(start=True) # builds the header of the html page
+    for i in range(int(client_args.count)):
+        request_data = [] # this list will generate a request for NASA API
 
-"""]
-_history = []
-print(choice(_logos))
-nasa = Scrape(api_key=client_args.key)
-if client_args.build:
-    nasa.build(start=True)
-for i in range(int(client_args.count)):
-    request_data = []
-    if (client_args.year == "random"): request_data.append(randint(1996, int((str(datetime.now()).split(" ")[0]).split("-")[0])))
-    else: request_data.append(client_args.year)
-    if (client_args.month == "random"): request_data.append(randint(1, 12))
-    else: request_data.append(client_args.year)
-    if (client_args.day == "random"): request_data.append(randint(1, 31))
-    else: request_data.append(client_args.year)
-    
-    output = nasa.request(year=str(request_data[0]), month=str(request_data[1]), day=str(request_data[2]))
-    if output not in _history:
-        _history.append(output)
-        if client_args.build:
-            nasa.build(output)
-            print('[  openNasa  ] Successfully added an article to the html page')
-        else:
-            print(output)
-else:
-    if client_args.build:
-        nasa.build(end=True)
+        if (client_args.year == "random"): request_data.append(randint(1996, int((str(datetime.now()).split(" ")[0]).split("-")[0])))
+        else: request_data.append(client_args.year)
+        if (client_args.month == "random"): request_data.append(randint(1, 12))
+        else: request_data.append(client_args.year)
+        if (client_args.day == "random"): request_data.append(randint(1, 31))
+        else: request_data.append(client_args.year)
+        
+        output = nasa.request(year=str(request_data[0]), month=str(request_data[1]), day=str(request_data[2])) # sending a request
+        if output not in _history: # check if there was such a request in the history
+            _history.append(output)
+            if loads(client_args.build.lower()):
+                nasa.build(output) # add an article to an html page
+                print('[  openNasa  ] Successfully added an article to the html page')
+            else:
+                print(output)
+    else:
+        if loads(client_args.build.lower()):
+            nasa.build(end=True) # will finish the html page
+
+if __name__ == "__main__":
+    nasa = Scrape(api_key=client_args.key)
+    client(nasa=nasa) # run a client
